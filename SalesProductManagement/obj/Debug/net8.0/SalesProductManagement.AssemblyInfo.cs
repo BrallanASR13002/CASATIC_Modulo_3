c719cc07a83ec2583b8dbc2f95316eb3f98283cc
BrallanASR13002 <sr13002@ues.edu.sv>
2025-02-19 10:33:03 -0600

    Elaboracion de los modelos

--- SalesProductManagement/obj/Debug/net8.0/SalesProductManagement.AssemblyInfo.cs
+++ SalesProductManagement/obj/Debug/net8.0/SalesProductManagement.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SalesProductManagement")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6569ad1710b8e99e87952f7ad486152217bfb25d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a04ba0cb195ec867e6306850ff0eb8cd082a6c35")]
 [assembly: System.Reflection.AssemblyProductAttribute("SalesProductManagement")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SalesProductManagement")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
