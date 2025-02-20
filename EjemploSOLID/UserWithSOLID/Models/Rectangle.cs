86b525045991f18b899ddabce32e127ebe3d4b63
BrallanASR13002 <sr13002@ues.edu.sv>
2025-02-20 13:05:19 -0600

    agregando comentarios con la herramienta nativa de vscode

--- EjemploSOLID/UserWithSOLID/Models/Rectangle.cs
+++ EjemploSOLID/UserWithSOLID/Models/Rectangle.cs
@@ -1,10 +1,15 @@
 //
 public class Rectangle : IShape
 {
-
+/// <summary>
+/// implements IShape interface
+/// </summary>
     public double Width { get; set; }
     public double Height { get; set; }
-
+/// <summary>
+/// method for to calculate the rectangle's area
+/// </summary>
+/// <returns>the rectangle's area</returns>
     public double CalculateArea()
     {
         return Height * Width;
--- EjemploSOLID/UserWithSOLID/Services/UserService.cs
+++ EjemploSOLID/UserWithSOLID/Services/UserService.cs
@@ -1,5 +1,13 @@
+/// <summary>
+/// user service, for to save
+/// user data and send emails
+/// </summary>
 public class UserService
 {
+    /// <summary>
+    /// save user data and send emails
+    /// </summary>
+    /// <param name="user">send a email from user</param>
     public void SaveDataUser(User user)
     {
         _ = new EmailSender();
