f6cbe7c17bdf6ccb59ed4aa09edd29f50dde3577
BrallanASR13002 <sr13002@ues.edu.sv>
2025-02-26 13:28:11 -0600

    Add documentation and improve connection string formatting in AppDbContext

--- ConnectToDataBaseExample/Program.cs
+++ ConnectToDataBaseExample/Program.cs
@@ -1,18 +1,18 @@
-﻿using System;
-using System.Linq;
-
+﻿// Purpose: Display the users table from the database
 class Program
 {
     static void Main()
-    {
+    {   // Display the users table
         Console.WriteLine("Users Table:");
         Console.WriteLine("____________________________________________________________");
+        // using the AppDbContext class, for the connection to the database
         using (var context = new AppDbContext())
         {
+            // get the users table from the database
             var users = context.users.ToList();
-
+            //define the users table and display the user ID and user name
             foreach (var user in users)
-            { 
+            {
                 Console.WriteLine($"User ID: {user.UserID} || User Name: {user.UserName}");
             }
         }
