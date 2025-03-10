27606b9ba49e2598ea56ce62e0cc829a2a89f010
BrallanASR13002 <sr13002@ues.edu.sv>
2025-03-10 13:09:41 -0600

    Refactor Program.cs to improve service instance definitions and update assembly informational version for consistency

--- EjercicioGuia1-7/Program.cs
+++ EjercicioGuia1-7/Program.cs
@@ -2,13 +2,21 @@
 {
     private static void Main(string[] args)
     {
+        //Define the getBoolDataService instance
         GetBoolDataService getBoolDataService = new GetBoolDataService();
+        //define the saleService instance
         SaleService saleService = new SaleService();
+        //define getNumericDataService instance
         GetNumericDataService getNumericDataService = new GetNumericDataService();
+        //define the getStringDataService instance
         GetStringDataService getStringDataService = new GetStringDataService();
+        //define the subSaleService instance and use the getNumericDataServic and
+        //getStringDataService instance as parameters
         SubSaleService subSaleService = new SubSaleService(getNumericDataService, getStringDataService, saleService);
-
+        //define the orderService instance and use getBoolDataService,   saleService and
+        //saleService instance as parameters
         OrderService orderService = new OrderService(getBoolDataService, saleService, subSaleService);
+        //calling the Cancontinue method
         orderService.CanContinue();
     }
 }
--- EjercicioGuia1-7/obj/Debug/net8.0/EjercicioGuia1-7.AssemblyInfo.cs
+++ EjercicioGuia1-7/obj/Debug/net8.0/EjercicioGuia1-7.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EjercicioGuia1-7")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9987878883918d7635bae1519dd52b4ae8d40e3d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2560d61b66752d76f7a5bfab0eb40f1ff8b00142")]
 [assembly: System.Reflection.AssemblyProductAttribute("EjercicioGuia1-7")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EjercicioGuia1-7")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
