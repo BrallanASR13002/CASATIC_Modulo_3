42be53e6f29912f20f07415f48120533081bbf5d
BrallanASR13002 <sr13002@ues.edu.sv>
2025-03-10 19:35:26 -0600

    Update assembly informational version and cache file for consistency

--- EjemploSOLID/UserWithOutSOLID/obj/Debug/net8.0/UserWithOutSOLID.AssemblyInfo.cs
+++ EjemploSOLID/UserWithOutSOLID/obj/Debug/net8.0/UserWithOutSOLID.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UserWithOutSOLID")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+81434b40e4c82b27b718061fbd5918b47d272df9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+27606b9ba49e2598ea56ce62e0cc829a2a89f010")]
 [assembly: System.Reflection.AssemblyProductAttribute("UserWithOutSOLID")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UserWithOutSOLID")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
